7809d26f2cada72d4e96f11f1bf2c1ea87aeb8c9
norjira <norjira@gmail.com>
2021-09-21 11:38:09 +0700

    Update Startup file

--- NewCore.API/Startup.cs
+++ NewCore.API/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NewCore.Data;
 
 namespace NewCore.API
 {
@@ -26,7 +28,36 @@ namespace NewCore.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            // services.AddControllers();
+            services.AddControllers(options =>
+               options.SuppressAsyncSuffixInActionNames = false
+            ).AddJsonOptions(options =>
+               options.JsonSerializerOptions.WriteIndented = true
+            );
+
+            // Add CORS
+            services.AddCors(options => {
+                options.AddDefaultPolicy(
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod()
+                    // with specify origin "http:.... must not end with '/'
+                    // builder => builder.WithOrigins(
+                    //     "http://localhost:5001",
+                    //     "http://localhost:5100"
+                    //     )
+                    //     .WithMethods("GET","PUT","POST","DELETE")
+                );
+                // 
+                // options.AddPolicy("mypolicy",
+                //     builder => builder.WithOrigins("http://localhost:5001")                    
+                // );
+
+                // Add Data Layer
+                services.AddDbContext<NewCoreDataContext>(options =>
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("TestDBConnection"));
+                });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +71,10 @@ namespace NewCore.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            // NOR - middleware CORS must be after userouting() and before useauthorization()
+            app.UseCors();  // use default
+            //app.UseCors("mypolicy");  // use mypolicy
 
             app.UseAuthorization();
 
